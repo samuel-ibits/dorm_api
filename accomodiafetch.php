cec3de12c6987ba1c0c2e6480d223b9d3387cf68
samuel ibitoye <98920110+samuel-ibits@users.noreply.github.com>
2022-06-06 23:38:18 +0100

    Update accomodiafetch.php

--- accomodiafetch.php
+++ accomodiafetch.php
@@ -4,34 +4,18 @@ include 'tokenizer.php';
 
 if(validatetoken($_COOKIE['dormtoken'])== 1){
 
-  <?php include 'connect.php';
-include 'tokenizer.php';
 
 
-if(validatetoken($_COOKIE['dormtoken'])== 1){
 
-  
-
-
-$selr="SELECT * FROM products";
-$result= $conn20->query($selr);
-If ($result->num_rows>0){  
-While ($row=$result->fetch_assoc()){
-	
-	$sn=$row["sn"];
-	$id=$row["id"];
-    $price=$row["price"];
-    $images=$row["images"];
-    $name=$row["name"];
-    $description=$row["description"];
-    
-
-    $cartegories=$row["cartegories"];
-    $merchant=$row["merchant"];
-
-    
-    $links=$row["links"];
-    
+    $selr="SELECT * FROM product";
+    $result= $conn14->query($selr);
+      If ($result->num_rows>0){
+    While ($row=$result->fetch_assoc()){
+    $id=$row["id"]; $name=$row["name"]; $price=$row["price"]; $about=$row["about"];
+    $pic1=$row["pic1"]; $pic2=$row["pic2"]; $pic3=$row["pic3"]; $pic4=$row["pic4"]; 
+    $pic5=$row["pic5"]; $pic6=$row["pic6"]; $vid1=$row["vid1"]; $vid2=$row["vid2"]; $vid3=$row["vid3"]; $vid4=$row["vid4"];
+    $vid5=$row["vid5"]; $vid6=$row["vid6"]; $messageid=$row["messageid"];
+    $status=$row["status"]; $views=$row["views"]; $contact=$row["contact"]; $link=$row["link"];
     
     $json_response = json_encode($row);
 
@@ -43,32 +27,3 @@ While ($row=$result->fetch_assoc()){
 }
 ?>
 
-
-
-$selr="SELECT * FROM products";
-$result= $conn20->query($selr);
-If ($result->num_rows>0){  
-While ($row=$result->fetch_assoc()){
-	
-	$sn=$row["sn"];
-	$id=$row["id"];
-    $price=$row["price"];
-    $images=$row["images"];
-    $name=$row["name"];
-    $description=$row["description"];
-    
-    $cartegories=$row["cartegories"];
-    $merchant=$row["merchant"];
-
-    $links=$row["links"];
-    
-    
-    $json_response = json_encode($row);
-
-    return $json_response; 
-}}else{
-}
-
-
-}
-?>
