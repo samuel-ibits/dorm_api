9dc401d2f4d6a9b61ff333ebcadeb8af62a03f37
samuel ibitoye <98920110+samuel-ibits@users.noreply.github.com>
2022-06-13 14:30:46 +0100

    Update blogssend.php

--- blogssend.php
+++ blogssend.php
@@ -4,25 +4,36 @@ include 'tokenizer.php';
 
 
   
-    $sn=$GET["sn"];
-	$id=$GET["id"];
-    $price=$GET["price"];
-    $images=$GET["images"];
-    $name=$GET["name"];
-    $description=$GET["description"];
-    
-    $cartegories=$GET["cartegories"];
-    $merchant=$GET["merchant"];
+   
+$blog_id=$_GET["blog_id"];
+$blog_title= $_GET["blog_title"];
+$blog_text= $_GET["blog_text"];
+$blog_img= $_GET["blog_img"];
+$blogger_name= $_GET["blogger_name"];
+$blog_post_date= $_GET["blog_post_date"];
 
-    $links=$GET["links"];
+function response($response_desc,$response_code){
+    $response['response_code'] = $response_code;
+	$response['response_desc'] = $response_desc;
+	$json_response = json_encode($response);
+	echo $json_response;
+
+}
     
 
 
 $sqllp = "INSERT INTO posts(blog_title,blog_text,blog_img,blogger_name,$blog_post_date,'$response_desc', '$response_code')VALUES ('$blog_title', '$blog_text', '$blog_img', '$blogger_name', '$blog_post_date', '$response_desc', '$response_code')";
 
-If ($GLOBALS['conn20']->query($sqllp) == TRUE) {
+If ($GLOBALS['conn21']->query($sqllp) == TRUE) {
   echo '<script> alert("products posted success")</script>';
-
+  
+  $response_desc='post sent sucessfully';
+    $response_code=200;
+response($response_desc,$response_code);
+} else{
+    $response_desc='post sending failed';
+    $response_code=500;
+response($response_desc,$response_code);
 }
 
 
