2e2e63b85ffc52ab14b9cf577191374fcee8c792
samuel ibitoye <98920110+samuel-ibits@users.noreply.github.com>
2022-06-24 06:06:13 +0100

    Update blogsfetch.php

--- blogsfetch.php
+++ blogsfetch.php
@@ -2,7 +2,7 @@
 header("Content-Type:application/json");
 
 header("Access-Control-Allow-Origin", "*");
-header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Methods', 'GET');
 
 include 'tokenizer.php';
 
