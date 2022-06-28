c32d0485d13c012999ea7f18eef2d67b3de6060e
samuel ibitoye <98920110+samuel-ibits@users.noreply.github.com>
2022-06-28 16:45:06 +0100

    Update signupapi.php

--- signupapi.php
+++ signupapi.php
@@ -1,4 +1,7 @@
 <?php include 'connect.php';
+
+header("Content-Type:application/json");
+
 // required headers
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
