031eb1fc8fd49bbd581b9485c98ffb175b329d85
samuel ibitoye <98920110+samuel-ibits@users.noreply.github.com>
2022-07-01 13:27:43 +0100

    Update texterapi.php

--- texterapi.php
+++ texterapi.php
@@ -3,10 +3,24 @@
 $phone=$_GET['phone'];
 $topic=$_GET['topic'];
 $message=$_GET['uname'];
-function texterapi($topic, $phone, $message){
-Echo"<fieldset style="."'"."visibility:hidden;padding:0%;margin:0px;height:0px;width:0px;"."'".">"."<embed src ="."'"."https://netbulksms.com/index.php?"."option=com_spc&comm=spc_api&username=dormcomn1&password=dormcomn1&sender=$topic&recipient=$phone&message=$message&
-    "."'".">";
-    Echo"</fieldset>";
 
+if(){
+    texterapi($topic, $phone, $message); 
 }
-?>
+
+function texterapi($topic, $phone, $message){
+
+$url = "https://netbulksms.com/index.php?"."option=com_spc&comm=spc_api&username=dormcomn1&password=dormcomn1&sender=".$topic."&recipient=".$phone."&message=".$message."";
+	
+	$client = curl_init($url);
+	curl_setopt($client,CURLOPT_RETURNTRANSFER,true);
+	$response = curl_exec($client);
+	
+	$result = json_decode($response);
+	
+	
+	
+	
+	   
+}
+	?>
