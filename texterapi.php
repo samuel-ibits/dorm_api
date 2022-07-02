c1808af0184c1ffd26667e086234abc80168e6ea
samuel ibitoye <98920110+samuel-ibits@users.noreply.github.com>
2022-07-02 11:20:10 +0100

    Update texterapi.php

--- texterapi.php
+++ texterapi.php
@@ -28,6 +28,7 @@ if($phone==""){}else{
 
 
 function texterapi($topic, $phone, $message){
+    echo'hi';
 Echo"<fieldset style="."'"."visibility:hidden;padding:0%;margin:0px;height:0px;width:0px;"."'".">"."<embed src ="."'"."https://netbulksms.com/index.php?option=com_spc&comm=spc_api&username=dormcomn1&password=dormcomn1&sender="."$topic"."&recipient="."$phone"."&message="."$message&
     "."'".">";
     Echo"</fieldset>";
