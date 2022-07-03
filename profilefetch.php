482cde03af993c612e24ecd97f09de0204284807
samuel ibitoye <98920110+samuel-ibits@users.noreply.github.com>
2022-07-03 18:08:51 +0100

    Update profilefetch.php

--- profilefetch.php
+++ profilefetch.php
@@ -101,7 +101,7 @@ $ihate=$row["ihate"];
 $icherish=$row["icherish"];
 $response["response_code"]=200;
     $response["response_desc"]="fetch sucessfull";
-    	echo $phone;
+    	echo 'fdjhf'.$phone.$name;
  responses($name,$username,$phone,$sta,$mcred,$course,$school,$email,$descyour,$year,$pocketid,$howsch,$descou,$dessch,$dob,$bescou,$besstudtm,$rescrush,$irep,$enjdoing,$favfood,$ihate,$icherish,$response_code,$response_desc);
     
 } 
